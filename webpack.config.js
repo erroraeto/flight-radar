091f01b141b0b6ae129bf49b78ce94087dd2bb2e
Danila Dippel <erroraeto@gmail.com>
2026-01-13 00:31:21 +0300

    theme, change mapstyle

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/flight-radar/',
   },
   module: {
     rules: [
